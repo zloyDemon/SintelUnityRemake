f93d3f39c854e24137d6f22b63207440ae387c4d
Marat Yakushev <yakushev1396@gmail.com>
2020-06-30 00:46:27 +0400

    Add folders and game manager

---
+++ Assets/Scripts/SintelGameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SintelGameManager : MonoBehaviour
+{
+    public static SintelGameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == this)
+            Destroy(gameObject);
+        if (Instance == null)
+            Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        InitSintelGame();
+    }
+
+    private void InitSintelGame()
+    {
+        Debug.Log("Sintel game initialization");
+    }
+}
