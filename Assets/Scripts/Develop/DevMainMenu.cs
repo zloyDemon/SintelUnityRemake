a553b4f090ed7c3cc5a00707c5487c657b6da225
Marat Yakushev <yakushev1396@gmail.com>
2020-09-04 23:25:37 +0400

    autoload

---
+++ Assets/Scripts/Develop/DevMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DevMainMenu : MonoBehaviour
+{
+    [SerializeField] Button dockButton;
+    [SerializeField] Button devButton;
+
+    private void Awake()
+    {
+        dockButton.onClick.AddListener(() => LoadLevel("docks_level"));
+        devButton.onClick.AddListener(() => LoadLevel("develop_scene"));
+    }
+
+    private void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
